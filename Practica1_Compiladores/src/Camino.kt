7b0e65575d58b8a01f8c2e6f9d72f0d4ac269162
Booz <33131059+boozinas@users.noreply.github.com>
2018-10-22 09:41:26 -0500

    Update Camino.kt

--- Practica1_Compiladores/src/Camino.kt
+++ Practica1_Compiladores/src/Camino.kt
@@ -1,3 +1,7 @@
+/*
+-Booz Medina
+-Generador dinamico de automatas
+*/
 class Camino(val estadoActual: Int, val simbolo : Char ){
 
     override fun toString()= "q" + estadoActual + "(" + simbolo + ")"
